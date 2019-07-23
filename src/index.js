23b0480e88c3ff049a1c6bde07ea74fd7618400e
root <root@DESKTOP-77RQ0M5.localdomain>
2019-07-23 13:38:31 -0300

    EsLint And Prettier

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#fff',
+  },
+
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: '#000',
+  },
+});
+
+const App = () => {
+  return (
+    <View style={styles.body}>
+      <Text style={styles.sectionDescription}>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
