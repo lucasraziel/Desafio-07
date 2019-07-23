19a958411c6bad9fa46f418f5fd9bc013a3b7755
root <root@DESKTOP-77RQ0M5.localdomain>
2019-07-23 13:59:58 -0300

    ReactNavigation

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: '#fff',
-  },
+import Routes from './routes';
 
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: '#000',
-  },
-});
-
-const App = () => {
-  return (
-    <View style={styles.body}>
-      <Text style={styles.sectionDescription}>Hello World</Text>
-    </View>
-  );
-};
-
-export default App;
+export default function App() {
+  return <Routes />;
+}
