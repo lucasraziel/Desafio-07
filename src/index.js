a84c43cb6f14fb0a26d3475ab72371f8c7651be7
root <root@DESKTOP-77RQ0M5.localdomain>
2019-07-23 13:52:29 -0300

    Reactotron

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import './config/ReactotronConfig';
 
 const styles = StyleSheet.create({
   body: {
