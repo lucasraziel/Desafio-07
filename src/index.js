0249b6c6b8188af0493c1a1d05f02d1b10df96f1
root <root@DESKTOP-77RQ0M5.localdomain>
2019-07-25 20:35:11 -0300

    Navegando Saga

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
 import './config/ReactotronConfig';
 
 import store from './store';
 
 import Routes from './routes';
+import NavigationService from './services/navigation';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Routes />
+      <StatusBar barStyle="light-content" />
+      <Routes
+        ref={navigatorRef => NavigationService.setNavigator(navigatorRef)}
+      />
     </Provider>
   );
 }
