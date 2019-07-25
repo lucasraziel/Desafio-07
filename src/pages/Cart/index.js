f715472f77630ea9ef534e5d5ce4e053b027cb7e
root <root@DESKTOP-77RQ0M5.localdomain>
2019-07-25 11:00:03 -0300

    Listando Carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -26,102 +28,56 @@ import {
 
 import { formatPrice } from '../../util/format';
 
-export default class Cart extends Component {
-  state = {
-    shopList: [
-      {
-        id: 1,
-        title: 'Tênis de Caminhada Leve Confortável',
-        price: 179.9,
-        image:
-          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
-      },
-      {
-        id: 2,
-        title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
-        price: 139.9,
-        image:
-          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
-      },
-      {
-        id: 3,
-        title: 'Tênis Adidas Duramo Lite 2.0',
-        price: 219.9,
-        image:
-          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
-      },
-      {
-        id: 5,
-        title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
-        price: 139.9,
-        image:
-          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
-      },
-      {
-        id: 6,
-        title: 'Tênis Adidas Duramo Lite 2.0',
-        price: 219.9,
-        image:
-          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
-      },
-      {
-        id: 4,
-        title: 'Tênis de Caminhada Leve Confortável',
-        price: 179.9,
-        image:
-          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
-      },
-    ],
-  };
-
-  render() {
-    const { shopList } = this.state;
-    return (
-      <Container>
-        <CartContainer>
-          <CartList
-            data={shopList}
-            keyExtractor={(item, index) => `list - item - ${index}`}
-            renderItem={({ item }) => (
-              <ItemCart>
-                <ItemDetail>
-                  <ImageProduct source={{ uri: item.image }} />
-                  <Description>
-                    <ProductDescription>{item.title}</ProductDescription>
-                    <Price>{formatPrice(item.price)}</Price>
-                  </Description>
+function Cart({ cart }) {
+  return (
+    <Container>
+      <CartContainer>
+        <CartList
+          data={cart}
+          keyExtractor={(item, index) => `list - item - ${index}`}
+          renderItem={({ item }) => (
+            <ItemCart>
+              <ItemDetail>
+                <ImageProduct source={{ uri: item.image }} />
+                <Description>
+                  <ProductDescription>{item.title}</ProductDescription>
+                  <Price>{formatPrice(item.price)}</Price>
+                </Description>
+                <TouchableOpacity>
+                  <Icon name="remove-shopping-cart" size={24} color="#7159c1" />
+                </TouchableOpacity>
+              </ItemDetail>
+              <PriceAndQuantity>
+                <Quantity>
                   <TouchableOpacity>
-                    <Icon
-                      name="remove-shopping-cart"
-                      size={24}
-                      color="#7159c1"
-                    />
+                    <Icon name="add-circle" size={18} color="#7159c1" />
                   </TouchableOpacity>
-                </ItemDetail>
-                <PriceAndQuantity>
-                  <Quantity>
-                    <TouchableOpacity>
-                      <Icon name="add-circle" size={18} color="#7159c1" />
-                    </TouchableOpacity>
-                    <InputTextQuantity value="3" />
-                    <TouchableOpacity>
-                      <Icon name="remove-circle" size={18} color="#7159c1" />
-                    </TouchableOpacity>
-                  </Quantity>
-                  <Subtotal>{formatPrice(543)}</Subtotal>
-                </PriceAndQuantity>
-              </ItemCart>
-            )}
-          />
-          <TotalContainer>
-            <TotalText>Total</TotalText>
-            <TotalPrice>{formatPrice(2500)}</TotalPrice>
-            <ButtonFinish>
-              <ButtonFinishText>FINALIZAR PEDIDO</ButtonFinishText>
-            </ButtonFinish>
-          </TotalContainer>
-        </CartContainer>
-      </Container>
-    );
-  }
+                  <InputTextQuantity value="3" />
+                  <TouchableOpacity>
+                    <Icon name="remove-circle" size={18} color="#7159c1" />
+                  </TouchableOpacity>
+                </Quantity>
+                <Subtotal>{formatPrice(543)}</Subtotal>
+              </PriceAndQuantity>
+            </ItemCart>
+          )}
+        />
+        <TotalContainer>
+          <TotalText>Total</TotalText>
+          <TotalPrice>{formatPrice(2500)}</TotalPrice>
+          <ButtonFinish>
+            <ButtonFinishText>FINALIZAR PEDIDO</ButtonFinishText>
+          </ButtonFinish>
+        </TotalContainer>
+      </CartContainer>
+    </Container>
+  );
 }
+
+Cart.propTypes = PropTypes.array.isRequired;
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
